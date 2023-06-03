f1ce6f56611ce0820af2b03f4d4bc29a8ee15512
Denis <drakonniravgol@gmail.com>
2023-06-03 18:07:58 +0300

    Added global search

--- my_shop/views.py
+++ my_shop/views.py
@@ -31,20 +31,21 @@ class ProductListView(ListView):
     def get_queryset(self):
         self.category_name = self.kwargs.get('category')
         self.sex_name = self.kwargs.get('sex')
-        self.sizes = list(
-            ProductSize.objects.filter(product__category__slug=self.category_name).values_list('size__name',
-                                                                                               flat=True).distinct()
-        )
+        if self.category_name == 'global-search':
+            self.sizes = list(ProductSize.objects.all().values_list('size__name', flat=True).distinct())
+        else:
+            self.sizes = list(ProductSize.objects.filter(product__category__slug=self.category_name).
+                              values_list('size__name', flat=True).distinct())
 
         # я как-то получаю фильтры и формурую из них словарь (значения по умолчанию должны быть False)
         # макет весьма плох, я не знаю, как получить данные из фильтра
         # можно было бы сделать кнопку "сохранить фильтр" которая передавала бы в get запросе выбранные пункты
         # чтобы работала моя фильтрация, достаточно как раз получить от клиента необходимые списки:
-        size_list = []
-        brand = []
-        designer = []
-
-        search = 'winter'.lower().strip()
+        size_list = self.request.GET.get('size_list').split(',') if self.request.GET.get('size_list') else []
+        brand = self.request.GET.get('brand').split(',') if self.request.GET.get('brand') else []
+        designer = self.request.GET.get('designer').split(',') if self.request.GET.get('designer') else []
+        search = self.request.GET.get('search', default='').lower().strip()
+        # print(size_list, designer, brand, search, sep='\n')
 
         filters = {
             'size_list': size_list,
@@ -53,16 +54,22 @@ class ProductListView(ListView):
         }
 
         queryset_filter = {
-            'title__icontains': search,
-            'category__slug': self.category_name,
-            'sex__short_name': self.sex_name,
             'is_active': True,
             'productsize__size__name__in': filters['size_list'] if filters['size_list'] else self.sizes,
         }
-        for f, value in filters.items():
-            if value:
-                queryset_filter[f'{f}__name__in'] = value
+        if search:
+            print('yes')
+            queryset_filter['title__icontains'] = search
+        else:
+            print('no')
+            queryset_filter['category__slug'] = self.category_name
+            queryset_filter['sex__short_name'] = self.sex_name
 
+            for f, value in filters.items():
+                if value:
+                    queryset_filter[f'{f}__name__in'] = value
+
+        print(queryset_filter)
         return Product.objects.filter(
             **queryset_filter
         ).annotate(total_quantity=Sum('productsize__quantity')).filter(total_quantity__gt=0)
